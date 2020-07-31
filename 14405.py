998b1e841d15e4ee5944b09d0e8e37647eaae635
Turtle-Hwan <67897841+Turtle-Hwan@users.noreply.github.com>
2020-07-31 12:23:07 +0900

    Update 14405.py
    파이썬 내장함수 문자열.count 사용

--- 14405.py
+++ 14405.py
@@ -2,32 +2,7 @@
 
 S = input()
 
-def pikachu():
-    if(S[0:2] == 'pi'):
-        if(len(S) == 2):
-            print('YES')
-            return
-        else:
-            S.lstrip('pi')
-            pikachu()
-            
-    elif(S[0:2] == 'ka'):
-        if(len(S) == 2):
-            print('YES')
-            return
-        else:
-            S.lstrip('ka')
-            pikachu()
-            
-    elif(S[0:3] == 'chu'):
-        if(len(S) == 3):
-            print('YES')
-            return
-        else:
-            S.lstrip('chu')
-            pikachu()
-    else:
-        print('NO')
-        return
-
-pikachu()
+if (S.count('pi')*2 + S.count('ka')*2 + S.count('chu')*3 == len(S)):
+    print("YES")
+else:
+    print("NO")
