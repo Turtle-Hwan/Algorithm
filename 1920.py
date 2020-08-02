b9fd4284a5929dc66fdb4a45fe87e94684c0d816
Turtle-Hwan <67897841+Turtle-Hwan@users.noreply.github.com>
2020-08-03 06:09:54 +0900

    Add files via upload
    filter 로 자르기 -> 시간초과

--- 1920.py
+++ 1920.py
@@ -1,6 +1,6 @@
 #1920번 수 찾기
 
-'''이분탐색'''
+'''이분탐색: 주어진 배열의 중앙값을 기준으로 찾는 값이 포함되는 부분만 계속 절반으로 나누어 가는 방법'''
 
 import sys
 input = sys.stdin.readline
@@ -21,9 +21,15 @@ for i in range(M):
     if(num[i] < A[0] or num[i] > A[N-1]):
         answer.append(0)
     else:
-        if(num[i] in A):
-            answer.append(1)
-        else:
-            answer.append(0)
-
+        B = A
+        while(True):
+            m = len(B)//2
+            if(num[i] == B[m]):
+                answer.append(1)
+                break
+            elif(num[i] > B[m]):
+                B = list(filter(lambda x: x > A[m], A))
+            elif(num[i] < B[m]):
+                B = list(filter(lambda x: x < A[m], A))
+        
 [print(i) for i in answer]
