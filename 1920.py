48357267cf685c77ecf2a6f98b78e9576c65a5a8
Turtle-Hwan <67897841+Turtle-Hwan@users.noreply.github.com>
2020-08-04 22:32:53 +0900

    **
    이분탐색(이진탐색) 구현 --> 시간 초과

--- 1920.py
+++ 1920.py
@@ -1,6 +1,6 @@
 #1920번 수 찾기
 
-'''이분탐색: 주어진 배열의 중앙값을 기준으로 찾는 값이 포함되는 부분만 계속 절반으로 나누어 가는 방법'''
+'''이분탐색(이진 탐색): 주어진 배열의 중앙값을 기준으로 찾는 값이 포함되는 부분만 계속 절반으로 나누어 가는 방법'''
 
 import sys
 input = sys.stdin.readline
@@ -22,14 +22,20 @@ for i in range(M):
         answer.append(0)
     else:
         B = A
+        end = N-1
+        start = 0
+        
         while(True):
-            m = len(B)//2
-            if(num[i] == B[m]):
+            m = (start+end)//2
+            if(start == end and num[i] != B[m]):
+                answer.append(0)
+                break
+            elif(num[i] == B[m]):
                 answer.append(1)
                 break
             elif(num[i] > B[m]):
-                B = list(filter(lambda x: x > A[m], A))
+                start = m+1
             elif(num[i] < B[m]):
-                B = list(filter(lambda x: x < A[m], A))
-        
+                end = m-1
+            
 [print(i) for i in answer]
