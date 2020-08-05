c188e18eaec5aaec28d7b318be57058e6948b8e2
Turtle-Hwan <67897841+Turtle-Hwan@users.noreply.github.com>
2020-08-05 20:19:45 +0900

    **
    set 자료형 이용 추가.

--- 1920.py
+++ 1920.py
@@ -15,27 +15,46 @@ num = list(map(int, input().split()))
 
 A.sort()
 
-answer = list()
-
 for i in range(M):
     if(num[i] < A[0] or num[i] > A[N-1]):
-        answer.append(0)
+        print(0)
     else:
-        B = A
         end = N-1
         start = 0
         
         while(True):
             m = (start+end)//2
-            if(start == end and num[i] != B[m]):
-                answer.append(0)
+            if(start == end and num[i] != A[m]):
+                print(0)
                 break
-            elif(num[i] == B[m]):
-                answer.append(1)
+            elif(num[i] == A[m]):
+                print(1)
                 break
-            elif(num[i] > B[m]):
+            elif(num[i] > A[m]):
                 start = m+1
-            elif(num[i] < B[m]):
+            elif(num[i] < A[m]):
                 end = m-1
-            
-[print(i) for i in answer]
+
+
+
+
+'''
+#set() 자료형 이용 (x in set() --> O(1))
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+A = set(input().split())
+
+M = int(input())
+
+num = input().split()
+
+for i in range(M):
+    if(num[i] in A):
+        print(1)
+    else:
+        print(0)
+'''
